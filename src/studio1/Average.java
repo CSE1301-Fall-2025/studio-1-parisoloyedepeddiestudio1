1c3a969693a5f023e7b2d57ffa3244dbe0b3b678
jonno <jonnoparis@gmail.com>
2025-09-03 15:43:01 -0500

    Studio1Finished

---
+++ src/studio1/Average.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+		System.out.print("What is your first number?");
+		int n1 = in.nextInt();
+		System.out.print("What is your second number?");
+		int n2 = in.nextInt();
+        double Average = (n1 + n2) / 2.0 ; 
+        System.out.println(Average);
+
+    }
+}
---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,15 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+		System.out.print("Provide me with a year.");
+		int n1 = in.nextInt();
+        boolean n1%4
+
+
+    }
+
+}
